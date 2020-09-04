a1ebe6670f3d2e42077c8ddf6e56d6d976e0d8c1
damonbogich <damonbogich@gmail.com>
2020-09-04 11:45:10 -0400

    problem 3 working

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -2,7 +2,25 @@ def intersection(arrays):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    total_lists = len(arrays)
+    #make empty list to return
+    result = []
+    #make dictionary to store the count of each number
+    counter_dict = {}
+
+    for i in arrays:
+        for j in i:
+            if j not in counter_dict:
+                counter_dict[j] = 1
+            else:
+                counter_dict[j] += 1
+    #now we must check dictionary for keys with value = length of arrays
+    dict_items_list = list(counter_dict.items())
+
+    for pair in dict_items_list:
+        if pair[1] == total_lists:
+            result.append(pair[0])
+
 
     return result
 
